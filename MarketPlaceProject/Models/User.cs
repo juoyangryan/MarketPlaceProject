117a46792c7cc8b583bcae248c77db990591fb42
juoyangryan <juoyangryan@gmail.com>
2024-03-25 22:53:24 -0700

    Add Login and Register Pages
    - Resolved styling issues

---
+++ MarketPlaceProject/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketPlaceProject.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Username Required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email Required")]
+        [EmailAddress(ErrorMessage = "Must be a valid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password confirmation missing")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
+        public string ConfirmPassword { get; set; }
+
+        //public byte[] ProfilePicture { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
