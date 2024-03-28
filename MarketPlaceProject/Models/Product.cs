02a2596aa4dae537952cda4bd79e80917aaaf4c5
juoyangryan <juoyangryan@gmail.com>
2024-03-28 16:40:26 -0700

    Add Product cards to Result View and dummy data in Controller

---
+++ MarketPlaceProject/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketPlaceProject.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Manufacture { get; set; }
+        public string Series { get; set; }
+        public string Model { get; set; }
+        public string UseType { get; set; }
+        public string Application { get; set; }
+        public string MountingLocation { get; set; }
+        public string Accessories { get; set; }
+        public int ModelYear { get; set; }
+        public double Power { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
